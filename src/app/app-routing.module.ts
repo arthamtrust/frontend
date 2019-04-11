f574b1fbe33ff3d4abb4578052b81cd413fb62fc
Vinoth Pandian <pandian@fit.fraunhofer.de>
2019-04-11 13:12:02 +0200

    Students page routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,21 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import {HomeComponent} from './home/home.component';
-import {HTTP404Component} from './core/http404/http404.component';
+import { HomeComponent } from './home/home.component';
+import { HTTP404Component } from './core/http404/http404.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: '/home'},
-  {path: 'home', component: HomeComponent},
-  {path: '**', component: HTTP404Component},
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'students',
+    loadChildren: './students/students.module#StudentsModule',
+  },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: '**', component: HTTP404Component },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- src/app/students/students.module.ts
+++ src/app/students/students.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StudentsRoutingModule } from './students-routing.module';
+import { StudentsComponent } from './students.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    StudentsRoutingModule
-  ]
+  declarations: [StudentsComponent, StudentDetailComponent],
+  imports: [CommonModule, StudentsRoutingModule],
 })
-export class StudentsModule { }
+export class StudentsModule {}
