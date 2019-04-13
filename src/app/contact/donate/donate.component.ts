2347bd2ff1ca87886eb00c1e6f5239f788a58bef
Vinoth <vinoth@Sarahs-MacBook-Pro.local>
2019-04-13 18:01:19 +0200

    Contact and donate page complete + gallery fix

--- src/app/contact/donate/donate.component.ts
+++ src/app/contact/donate/donate.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Details {
+  name: string;
+  detail: string;
+}
+
 @Component({
   selector: 'app-donate',
   templateUrl: './donate.component.html',
-  styleUrls: ['./donate.component.scss']
+  styleUrls: ['./donate.component.scss'],
 })
 export class DonateComponent implements OnInit {
+  details: Details[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.details = [
+      {
+        name: 'Bank',
+        detail: 'HDFC',
+      },
+      {
+        name: 'Name',
+        detail: 'ARTHAM CHARITABLE FOUNDATION',
+      },
+      {
+        name: 'Account Number',
+        detail: '50200000458360',
+      },
+      {
+        name: 'IFSC code',
+        detail: 'HDFC0002409',
+      },
+      {
+        name: 'Account type',
+        detail: 'Current account',
+      },
+      {
+        name: 'Branch',
+        detail: 'Bye pass, Madurai',
+      },
+    ];
   }
 
+  ngOnInit() {}
 }
